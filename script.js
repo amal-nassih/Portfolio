5edd444d7e2e87f241ddf132f8a35f7cf9e2116c
amal nassih <81804346+amal-nassih@users.noreply.github.com>
2022-04-29 11:27:20 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function showNav() {
+    document.getElementById('main-header').style.display = "flex";
+    document.getElementById('main-header').style.position = "fixed";
+    document.getElementById('main-header').style.top = "0px";
+    document.getElementById('main-header').style.zIndex = "5";
+ }
+ 
